88cbe3f5aaeb73bb3b19c780845e0c49f69dbd7f
shivashankarAlluri <ashivashankar1211@gmail.com>
2026-01-30 19:59:57 +0530

    creds

--- token.js
+++ token.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const app = express();
+const cors = require("cors");
+
 CLIENT_ID="cs-8382e2d8-23af-560c-b576-08cc20c52cb8"
 CLIENT_SECRET="LonqHE7zddsy01RkrCN0+o+bNqVQ51Vh2FdcJrRrlQU="
 BOT_ID="st-7ff44e13-62c3-57dc-a184-0fd8e4ff185b"
