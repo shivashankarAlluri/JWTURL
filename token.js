0631f3fee90f81ef27e4330ad74a3120402a69b1
shivashankarAlluri <ashivashankar1211@gmail.com>
2026-01-30 19:52:15 +0530

    creds

--- token.js
+++ token.js
@@ -4,6 +4,16 @@ const app = express();
 CLIENT_ID="cs-8382e2d8-23af-560c-b576-08cc20c52cb8"
 CLIENT_SECRET="LonqHE7zddsy01RkrCN0+o+bNqVQ51Vh2FdcJrRrlQU="
 BOT_ID="st-7ff44e13-62c3-57dc-a184-0fd8e4ff185b"
+
+app.use(
+  cors({
+    origin: "http://localhost:9000",
+    methods: ["POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use(express.json());
 app.post("/jwt", (req, res) => {
   const payload={
  "aud": "https://idproxy.kore.com/authorize",
